e9405e5f1d94f0dd16cd536c97a8218b43eef16c
Øyvind Granberg <granberg@tresfjording.no>
2025-12-25 01:39:19 +0100

    toppynt

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ function oppdaterInfo(entry) {
   document.getElementById('statusDisplay').textContent =
     `☑ Fant data for ${entry.tettsted}`;
 
-  document.getElementById("valgtKommuneDisplay").textContent = entry.kommune ?? 'Ukjent';
+  document.getElementById("valgtKommuneDisplay").textContent = entry.kommune;
   document.getElementById('k_nrDisplay').textContent = entry.k_nr ?? 'Ukjent';
   document.getElementById('tettstedDisplay').textContent = entry.tettsted ?? 'Ukjent';
   document.getElementById('fylkeDisplay').textContent = entry.fylke ?? 'Ukjent';
