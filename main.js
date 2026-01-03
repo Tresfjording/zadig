6f997aa70fe310646ab8bcec3baa7e9fd72f5728
Tresfjording <34656827+Tresfjording@users.noreply.github.com>
2026-01-04 00:19:46 +0100

    04.01.2026  - 00:19:45

--- main.js
+++ main.js
@@ -228,7 +228,7 @@ function renderAllHytteMarkers() {
 
         const marker = L.marker([lat, lon], {
             title: h.h_navn,
-            icon: img/hytteikon.xcf   // ← bruk trekanten her
+            icon: img/hytteikon.png   // ← bruk trekanten her
         });
 
         marker.on("mouseover", () => updateInfoBoxWithCabin(h));
