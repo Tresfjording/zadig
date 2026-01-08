43917c9f5698eda025e7310b834d0e4ab09921a0
Tresfjording <34656827+Tresfjording@users.noreply.github.com>
2026-01-08 06:51:14 +0100

    08.01.2026  - 06:50:56

--- main.js
+++ main.js
@@ -98,7 +98,7 @@ function initMap() {
 async function loadData() {
   try {
     const [samletResp, factsResp] = await Promise.all([
-      fetch("https://www.tresfjording.no/samlet.json"),
+      fetch("https://www.tresfjording.no/tettsteder_3.json"),
       fetch("facts_all.json"),
     ]);
 
